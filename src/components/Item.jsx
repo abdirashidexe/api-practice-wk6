7cb00d64692631db3f5898a976c1bf7c3a76bd36
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-10-28 13:50:49 -0700

    pushing

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,41 @@
 import Picture from "./Picture"
 import Info from "./Info"
+import { useState, useEffect } from 'react'
 
 export default function Item() {
-    return(
+    const [pokeId, setPokeId] = useState(1)
+    const [pokemon, setPokemon] = useState(null);
+    const [images, setImages] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+            .then(res => res.json())
+            .then(pokeData => setPokemon(pokeData));
+            setLoading(false);
+    }, [pokeId]);
+
+    console.log(pokemon);
+
+    if (pokemon !== null) setImages(pokemon.sprites.other['official-artwork']);
+
+    function showNext() {
+        setPokeId(prev => prev + 1);
+    }
+
+    if (loading) {
+        return (
+            <h1>Content is loading...</h1>
+        )
+    }
+
+    return (
         <>
             <p>this is an item component</p>
             <Picture />
             <Info/>
+            {pokemon !== null && <img src={pokemon.sprites.other['official-artwork'].front_default} alt="" />}
+            <button onClick={showNext}>Next</button>
         </>
     )
 }
