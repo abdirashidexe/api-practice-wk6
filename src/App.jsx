df839fb491ef262e52d24151fdee110827dc1c31
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2025-10-28 13:23:52 -0700

    changed main to item to meet step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header'
-import Main from './components/Main'
+import Item from './components/Item'
 import Footer from './components/Footer'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Header/>
-      <Main/>
+      <Item/>
       <Footer/>
     </>
   )
--- src/components/Main.jsx
+++
@@ -1,7 +0,0 @@
-export default function Main() {
-    return(
-        <>
-            <p>main content</p>
-        </>
-    )
-}
